c461f96866444c3b311c10b81c320e6c7ed39911
kyllian1212 <kyllian1212@live.fr>
2023-05-26 23:21:40 +0100

    fix explaination

--- device_NovationLaunchpadMiniMK3Daw.py
+++ device_NovationLaunchpadMiniMK3Daw.py
@@ -34,7 +34,7 @@ class Launchpad:
         print('midi id:', event.midiId, '| midi status:', event.status, '| midi channel:', event.midiChan, 
         '| midi data1:', event.data1, '| midi data2:', event.data2, '| midi controlNum:', event.controlNum, 
         '| midi controlVal:', event.controlVal, '| midi sysex:', event.sysex)
-        #device.midiOutMsg(state1(normal pad)/state2(CC Pad), state2(CC Pad)/state1(normal pad), pad, color)
+        #device.midiOutMsg(state1(normal pad)/state2(CC Pad), state1(CC Pad)/state2(normal pad), pad, color)
         #state1 and state2 need to be set together BUT switches up position depending on which type of pad you set
         #normal pads are the 64 main pads, cc pads are the top/right "setting" pads + novation logo
         #idk why it needs to be set like this but yeah
