c981649aef6735bf15f99e02f08010e6d3f81d4d
kyllian1212 <kyllian1212@live.fr>
2023-06-23 22:29:22 +0100

    fix midi

--- device_NovationLaunchpadMiniMK3Midi.py
+++ device_NovationLaunchpadMiniMK3Midi.py
@@ -1,8 +1,6 @@
 # name=Novation Launchpad Mini MK3 Midi
 import device
 
-import launchpad as lp
-
 class MidiLaunchpad:
     def __init__(self):
         self.tick = 0
@@ -10,23 +8,19 @@ class MidiLaunchpad:
     
     def OnInit(self):
         device.setHasMeters()
-        lp.enableDawMode()
 
     def OnDeInit(self):
-        lp.disableDawMode()
+        pass
     
     def OnIdle(self):
         self.tick += 1
         pass
 
     def OnMidiMsg(self, event):
-        event.handled = False
         print('--------------------------------')
         print('midi id:', event.midiId, '| midi status:', event.status, '| midi channel:', event.midiChan, 
         '| midi data1:', event.data1, '| midi data2:', event.data2, '| midi controlNum:', event.controlNum, 
         '| midi controlVal:', event.controlVal, '| midi sysex:', event.sysex)
-
-        event.handled = True
 
 midiLp = MidiLaunchpad()
 
